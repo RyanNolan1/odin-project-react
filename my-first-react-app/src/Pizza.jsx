2fd955d7222c775b666fbfbfeb993fa43b73d9e4
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2023-12-18 21:17:26 +0000

    this will add a pizza logo to the app

---
+++ my-first-react-app/src/Pizza.jsx
@@ -0,0 +1,7 @@
+import pizzaCartoon from "./assets/pizzaCartoon.jpg";
+
+function Pizza() {
+   return <img src={pizzaCartoon} alt="Pizza Cartoon" id="pizza-cartoon" />
+}
+
+export default Pizza;
--- my-first-react-app/src/main.jsx
+++ my-first-react-app/src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Greeting from './Greeting.jsx'
+import Pizza from './Pizza.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Greeting />
+    <Pizza />
   </React.StrictMode>,
 )
